fd5e09fa6a6b6dd8e3f8febb493e78ce175147f6
SietseAndringa <82322340+SietseAndringa@users.noreply.github.com>
2021-10-19 16:49:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const makePizza = function () {
+    console.log('Make bottom with dough');
+    console.log('Cover bottom with sauce');
+    console.log('Put on ingriedients');
+    console.log('Put it in the oven');  
+};
+
+const makeSushi = function () {
+    console.log('Boil rice');
+    console.log('Spread out rice');
+    console.log('Put on ingriedients');
+    console.log('Make roll and cut it into slices');  
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
